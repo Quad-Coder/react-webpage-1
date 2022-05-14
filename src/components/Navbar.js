de47bcf22bcc0ec918e15097b1a6071f84cecba4
Quadrick <quadtreeder@gmail.com>
2022-05-13 20:38:14 -0700

    started navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../videos/R.gif";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
 
 function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/R" className="navbar-logo">
-            <img src={logo} alt="loading..." />
-          </Link>
-          <div className="menu-icon">
-            <i className=""></i>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <div className="navbar-container">
+            <Link to="#" className="menu-bars">
+              <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
           </div>
         </div>
-      </nav>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineCloseCircle />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
---
+++ src/components/SidebarData.js
@@ -0,0 +1,37 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "About",
+    path: "/about",
+    icon: <IoIcons.IoMdPerson />,
+    cName: "nav-text",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+    icon: <AiIcons.AiOutlineFundProjectionScreen />,
+    cName: "nav-text",
+  },
+  {
+    title: "Photography",
+    path: "/photography",
+    icon: <AiIcons.AiFillCamera />,
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <IoIcons.IoMdPhonePortrait />,
+    cName: "nav-text",
+  },
+];
