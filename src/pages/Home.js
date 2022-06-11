417c8c17937511dcf6c96caa98c22d18a03a75d6
Quadrick <quadtreeder@gmail.com>
2022-06-11 12:30:09 -0700

    Added Logos & Profile Picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ProPic from "../components/images/";
+import ProPic from "../components/images/ProPic.JPG";
+import CodeSymbol from "../components/images/CodeSymbol.JPG";
+import ThinkfulLogo from "../components/images/ThinkfulLogo.JPG";
 
 function Home() {
   return (
@@ -8,19 +10,29 @@ function Home() {
         <h3 className="Intro" style={{ color: "#bfc0c0" }}>
           Hey! My name is Quadrick Reeder and I am you next Software Engineer.
         </h3>
-        <img src={ProPic} alt="Self Portrait" />
+        <img className="proPic" src={ProPic} />
       </div>
-      <p style={{ color: "#bfc0c0" }}>
-        My interest in coding started in 2018 and that same year I started doing
-        my research to dable with different coding languages. It wasn't until I
-        started taking classes at Laney College that I really started to apply
-        myself.{" "}
-      </p>
-      <p style={{ color: "#bfc0c0" }}>
-        In 2021 I attended Thinkful's Software Engineering Immersion Program. It
-        was here that I learned industry standards using the JavaScript language
-        different tools such as CSS, HTML, MongoDB, Express, and React.
-      </p>
+      <div className="Interest">
+        <img className="codeSymbol" src={CodeSymbol} />
+        <p style={{ color: "#bfc0c0" }}>
+          My interest in coding started in 2018 and that same year I started
+          doing my research to dable with different coding languages. It wasn't
+          until I started taking classes at Laney College that I really started
+          to apply myself.
+        </p>
+      </div>
+      <div className="Thinkful">
+        <p style={{ color: "#bfc0c0" }}>
+          In 2021 I attended Thinkful's Software Engineering Immersion Program.
+          It was here that I learned industry standards using the JavaScript
+          language different tools such as CSS, HTML, MongoDB, Express, and
+          React.
+        </p>
+        <img className="thinkfulLogo" src={ThinkfulLogo} />
+      </div>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
